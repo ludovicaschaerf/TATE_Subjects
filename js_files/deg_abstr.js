2b4efc85d00be7959a5c2fa59c0aa43706006bf7
ludovicaschaerf <ludovica.schaerf@gmail.com>
2021-06-03 19:02:56 +0200

    file

---
+++ js_files/deg_abstr.js
@@ -0,0 +1,4 @@
+deg_abstr = {"abstraction":0.8,"architecture":0.3,"emotions, concepts and ideas":0.9,
+"history":0.6,"interiors":0.4,"leisure and pastimes":0.8,"literature and fiction":0.7,
+"nature":0,"objects":0,"religion and belief":0.5,"society":0.6,"symbols & personifications":1,
+"work and occupations":0.7, "people":0, "places":0.2, "movement":1};
